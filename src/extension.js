9fd4aa8ff72deb49a7f02e41a96359763d58028e
utpalb <utpalb@altair.com>
2024-08-14 12:21:45 +0530

    Added r-click and cmd

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,27 @@
+const vscode = require('vscode');
+
+/**
+ * Will create the tree and print at the active editor cursor
+ * @param {Object} path 
+ * @returns {void}
+ */
+function generateFolderTreeHandler(uri) {
+	console.log(uri)
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
-function activate() {
+/**
+ * 
+ * @param {vscode.ExtensionContext} context 
+ * @returns 
+ */
+function activate(context) {
+	const generateFolderTree = 'ultree.generateFolderTree';
+
+	context.subscriptions.push(vscode.commands.registerCommand(generateFolderTree, generateFolderTreeHandler));
+
 	return {
 		extendMarkdownIt(md) {
 		  	return md.use(require('markdown-it-ultree'));
