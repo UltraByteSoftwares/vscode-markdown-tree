6efe444012a6649c39b84ca9aeb965823384e547
utpalb <utpalb@altair.com>
2024-08-19 17:14:12 +0530

    Updated options

--- src/fastglob-explorer.js
+++ src/fastglob-explorer.js
@@ -1,8 +1,20 @@
 // Uses the fast-glob package from npm
 const fg = require('fast-glob');
 const path = require('path');
+const OptionsManager = require('./options-manager.js')
 
 class FastGlobExplorer {
+    getDefaultOptions() {
+        return {
+            // Indirect options
+            recursive: true,
+            // Direct options
+            ignore: "",     /* semi-colon separated values */
+            deep: -1,
+            markDirectories: false
+        }
+    }
+
     /**
      * Gets all the files and folders in a path and returns
      * a sorted list of files
@@ -12,16 +24,21 @@ class FastGlobExplorer {
      * @returns {string[]} A flat list of files
      */
     async getFiles(folderpath, options = null) {
-        const opts = {
-            cwd: path.dirname(folderpath),
-            onlyFiles: false
-        };
-    
-        if (options)
-            Object.assign(opts, options);
+        const {ignore, recursive, deep, markDirectories} = 
+            OptionsManager.copyOptions(this.getDefaultOptions(), options);
     
         const baseName = path.basename(folderpath);
-        const files = await fg.async(`${baseName}/**`, opts);
+        const pattern = recursive ? '**' : '*';
+
+        const files = await fg.async(`${baseName}/${pattern}`, {
+            cwd: path.dirname(folderpath),
+            onlyFiles: false,
+            // User configurable options
+            ignore: ignore.split(';').map(s => s.trim()).filter(s => s !== ''),
+            deep: deep === -1 ? Infinity : deep,
+            markDirectories: markDirectories
+        });
+
         files.sort();
         files.unshift(baseName);
         
