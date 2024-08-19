6a546699ce4e7e947a7bc27200cb90cfb97140b1
utpalb <utpalb@altair.com>
2024-08-20 01:36:45 +0530

    Added decorator test

---
+++ test/tree-decorator.test.js
@@ -0,0 +1,51 @@
+const TreeDecorator = require('../src/tree-decorator');
+
+const assert = require('assert');
+
+suite('Tree decorator', () => {
+    // vscode.window.showInformationMessage('Start all tests.');
+    test('Sorted items', () => {
+        const input = [
+            'lorem',
+            '    ipsum',
+            '    dolor',
+            '        sit',
+            '            amet',
+            '                consectetur',
+            '            adipiscing',
+            '        elit',
+            '    sed ',
+            '        do ',
+            '        eiusmod ',
+            '        tempor ',
+            '            incididunt ',
+            '    ut ',
+            '    labore ',
+            '        et'
+        ];
+        
+        const ref = [
+            'lorem',
+            '├── ipsum',
+            '├── dolor',
+            '│   ├── sit',
+            '│   │   ├── amet',
+            '│   │   │   └── consectetur',
+            '│   │   └── adipiscing',
+            '│   └── elit',
+            '├── sed ',
+            '│   ├── do ',
+            '│   ├── eiusmod ',
+            '│   └── tempor ',
+            '│       └── incididunt ',
+            '├── ut ',
+            '└── labore ',
+            '    └── et'
+        ]
+
+        const dec = new TreeDecorator();
+        const output = dec.decorate(input);
+
+        assert.deepStrictEqual(ref, output);
+    });
+});
